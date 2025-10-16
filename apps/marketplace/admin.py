11d97644c4a9003f75915e30f3ccd64f46d59315
synzr <mikhail@synzr.space>
2025-10-16 10:20:59 +0500

    feat(apps/marketplace): all models exclude review

---
+++ apps/marketplace/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Distribution)
+class DistributionAdmin(admin.ModelAdmin):
+    pass
