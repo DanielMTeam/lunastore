608f12b0dde73b683e3057e62a58dd8ff7b16de3
fayzetwin <veryenten.xd@gmail.com>
2025-10-17 23:58:17 +0300

    feat: added cdn folder & added home page render (now home page works!!0!!)

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # views of home page 
 
+# redirect to home (index.php) page from (/) page
+def homeRedirect(request):
+    return redirect('/index.php')
+
+# home page
 def marketplace(request):
-    return HttpResponse("hello world!!!")
+    return render(request, 'index.html')
