ee0e66baa787a7b48137b2fe26bd7f4086494e5b
fayzetwin <veryenten.xd@gmail.com>
2025-10-18 22:15:38 +0300

    feat: is_under_dmca & is_demo app flags; added search apps by name in admin panel; fix: fix frontend in app page

--- apps/core/templatetags/app.py
+++ apps/core/templatetags/app.py
@@ -21,3 +21,29 @@ def show_app(app_id):
         download_url = None
 
     return {'app': app, 'screenshots': screenshots, 'download_url': download_url, 'app_id': app_id}
+
+@register.inclusion_tag('includes/demo_flag.html')
+def demo_visible(app_id):
+    app = None
+    demo = False
+    try:
+        app = Application.objects.get(id=app_id)
+        demo = app.is_demo
+    except Application.DoesNotExist:
+        app = None
+        demo = None
+    print(bool(demo))
+    return {'app': app, 'app_id': app_id, 'is_demo': bool(demo)}
+
+@register.inclusion_tag('includes/dmca_flag.html')
+def dmca_visible(app_id):
+    app = None
+    dmca = False
+    try:
+        app = Application.objects.get(id=app_id)
+        dmca = app.is_under_dmca
+    except Application.DoesNotExist:
+        app = None
+        dmca = None
+    print(bool(dmca))
+    return {'app': app, 'app_id': app_id, 'is_dmca': bool(dmca)}
