898d5ec3a4090c62b31b326b5d80c0b330d8db0a
fayzetwin1 <defactorioaspartame@gmail.com>
2026-02-03 10:15:00 +0300

    refactor: disable DRF frontend for API-methods (only-json)

--- lunastore/settings.py
+++ lunastore/settings.py
@@ -55,6 +55,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'EXCEPTION_HANDLER': 'apps.api.handlers.luna_exception_handler',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer', 
+    ),
 }
 
 SPECTACULAR_SETTINGS = {
