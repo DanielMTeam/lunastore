e3a7b282c0fb4515cf0fbb59e7ae4c6fc3222026
fayzetwin1 <defactorioaspartame@gmail.com>
2026-02-03 08:34:02 +0300

    feat: 'MarketplaceViewSet' class; 'getAppInfo' API method

--- apps/api/constants.py
+++ apps/api/constants.py
@@ -1,8 +1,14 @@
 class ErrorCodes:
+    # Main ErrorCode constants (0xxx like)
     EVERYTHING_IS_ALRIGHT = 0
     UNKNOWN_ERROR = 1
     VALIDATION_ERROR = 2
     NOT_FOUND = 3
     
+    # User ErrorCode constants (1xxx like)
     USER_NOT_FOUND = 1000
     USER_IS_BLOCKED = 1001
+    
+    # Application ErrorCode constants (2xxx like)
+    APPLICATION_NOT_FOUND = 2000
+    APPLICATION_IS_UNDER_DMCA = 2001
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from apps.user.models import User 
-from .serializers import Serializer
+from apps.marketplace.models import Application
+from apps.user.serializers import UserSerializer
+from apps.marketplace.serializers import ApplicationSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .exceptions import LunaException
@@ -10,7 +12,7 @@ from drf_spectacular.types import OpenApiTypes
 
 class UserViewSet(viewsets.GenericViewSet):
     queryset = User.objects.filter(is_active=True)
-    serializer_class = Serializer
+    serializer_class = UserSerializer
 
     @extend_schema(
         summary="get user profile info",
@@ -46,4 +48,49 @@ class UserViewSet(viewsets.GenericViewSet):
             )
 
         serializer = self.get_serializer(user)
+        return Response(serializer.data)
+    
+class MarketplaceViewSet(viewsets.GenericViewSet):
+    queryset = Application.objects.filter()
+    serializer_class = ApplicationSerializer
+
+    @extend_schema(
+        summary="get app info",
+        description="returns app info",
+        parameters=[
+            OpenApiParameter(
+                name='id',             
+                description='ID of application',
+                required=True,         
+                type=OpenApiTypes.INT,  
+                location=OpenApiParameter.QUERY
+            )
+        ]
+    )
+    @action(detail=False, methods=['get'], url_path='getAppInfo')
+    def get_app(self, request):
+        id = request.query_params.get('id')
+        
+        if not id:
+            raise LunaException(
+                code=ErrorCodes.VALIDATION_ERROR,
+                message="'ID' field missing",
+                status_code=400
+            )
+        try:
+            app = self.get_queryset().get(pk=id)
+        except Application.DoesNotExist: 
+            raise LunaException(
+                code=ErrorCodes.APPLICATION_NOT_FOUND,
+                message=f"Application with id {id} was not found",
+                status_code=404
+            )
+        if app.is_under_dmca:
+            raise LunaException(
+                code=ErrorCodes.APPLICATION_IS_UNDER_DMCA,
+                message=f"Application with id {id} is under DMCA strike",
+                status_code=403
+            )
+
+        serializer = self.get_serializer(app)
         return Response(serializer.data)
