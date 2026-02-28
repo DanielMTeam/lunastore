8d888de188cf87f102ef0d130f7d40fbcd2a3336
fayzetwin <fayzetwin.xd@gmail.com>
2026-02-28 08:39:14 +0300

    fix: STATIC_ROOT constant for CI/CD

--- lunastore/settings.py
+++ lunastore/settings.py
@@ -325,5 +325,6 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
