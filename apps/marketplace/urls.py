db22d7658275165382b270ec994c14118a881ad9
Roman Sergienko <romasergienko@outlook.com>
2026-03-13 00:28:23 +0300

    Revert "feat: добавлено управление дистрибуциями приложений и форма редактирования дистрибуций"
    This reverts commit 9f6831298fe333662d51a12bb3aefb6af2319fce.

--- apps/marketplace/urls.py
+++ apps/marketplace/urls.py
@@ -7,14 +7,10 @@ urlpatterns = [
     path('index.php', views.marketplace, name='index'),
     path('category.php', views.category, name='category'),
     path('app.php', views.app, name='app'),
-    path('download.php', views.download_list, name='download_list'),
     path('faq.php', views.faq, name='faq'),
     path('app_add.php', views.app_add, name='app_add'),
     path('settings_apps.php', views.settings_apps, name='settings_apps'),
     path('edit_app_info.php/<int:pk>/', views.application_edit_info, name='edit_app_info'),
-    path('distributions.php', views.manage_distributions, name='manage_distributions'),
-    path('distribution_edit.php/<int:dist_pk>/', views.distribution_edit, name='distribution_edit'),
-    path('distribution_delete.php/<int:dist_pk>/', views.distribution_delete, name='distribution_delete'),
     path('search.php', views.search, name='search'),
     path('report_app.php', views.report_app, name='report_app')
 ]
