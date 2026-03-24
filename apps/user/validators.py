a5a044510b0f6afd434d25b5d59ba663e8ebe35e
fayzetwin1 <defactorioaspartame@gmail.com>
2026-03-24 21:43:54 +0300

    remove head in conflicted files

--- apps/user/validators.py
+++ apps/user/validators.py
@@ -25,20 +25,11 @@ def validate_invite_limit(owner):
         days=int(settings.MAX_INVITE_DAYS_LIMIT)
     )
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> fd172f14a06083285c90c8803e5a2621ac2b6e3b
     recent_invites_count = User.objects.filter(
         invited_by=owner,
         date_joined__gte=time_threshold,
     ).count()
-<<<<<<< HEAD
 
-    if recent_invites_count >= settings.MAX_INVITE_USES_COUNT:
-=======
-    
     if recent_invites_count >= int(settings.MAX_INVITE_USES_COUNT):
->>>>>>> fd172f14a06083285c90c8803e5a2621ac2b6e3b
         return False
     return True
