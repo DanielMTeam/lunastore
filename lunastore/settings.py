7044007edcbdc58b4e4de0346af24dc768dc0ad7
fayzetwin <fayzetwin.xd@gmail.com>
2026-03-24 14:29:49 +0300

    fix: settings.py

--- lunastore/settings.py
+++ lunastore/settings.py
@@ -227,7 +227,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,gdsf
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
