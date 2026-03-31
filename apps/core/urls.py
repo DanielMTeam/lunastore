3d3734c1ce5c0913a6c22cfad4f1eacb7babd5e1
fayzetwin <fayzetwin.xd@gmail.com>
2026-03-31 21:08:46 +0300

    feat: added localization system;

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("other_projects.php", views.other_projects, name="other_projects"),
     path("help_center.php", views.help_center, name="help_center"),
     path("debug_info.php", views.debug_info, name="debug_info"),
+    path(
+        "set-lang/<str:lang_code>/",
+        views.force_language_change,
+        name="force_language_change",
+    ),
 ]
