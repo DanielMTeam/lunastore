8843de38f3093302826a1ebfb4c29a987c600d3b
fayzetwin1 <defactorioaspartame@gmail.com>
2026-05-17 19:00:06 +0300

    fix: debug settings variable

--- lunastore/settings.py
+++ lunastore/settings.py
@@ -42,7 +42,7 @@ LOGIN_REDIRECT_URL = "/index.php"
 
 LOGOUT_REDIRECT_URL = "/login.php"
 
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 TELEGRAM_LOGGER_ENABLED = os.getenv("TELEGRAM_LOGGER_ENABLED", "False") == "True"
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
