c2f63909e9423eab9201cf4d503975243e48b355
fayzetwin1 <defactorioaspartame@gmail.com>
2026-05-17 21:03:18 +0300

    fix: getPrivUploadToken url problem

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include 
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, MarketplaceViewSet, CategoryViewSet, ServiceViewSet, DistributionViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
+
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'marketplace', MarketplaceViewSet, basename='marketplace')
 router.register(r'category', CategoryViewSet, basename='category')
