9014196b01df6666131602df20eefc9c1f4442ed
fayzetwin1 <defactorioaspartame@gmail.com>
2026-05-20 23:15:52 +0300

    fix: whitenoise static storage backend

--- lunastore/settings.py
+++ lunastore/settings.py
@@ -575,7 +575,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
