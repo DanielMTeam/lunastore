2b513f5e32b0631a1b1a15fd6d288db6e767cc7d
loroteber <loroteber@vk.com>
2026-09-24 19:02:06 +0500

    fix: add content hashing for static

--- apps/core/staticfiles.py
+++ apps/core/staticfiles.py
@@ -5,7 +5,7 @@ class StaticFilesStorage(CompressedManifestStaticFilesStorage):
     def url(self, name, force=False):
         while name.startswith("./"):
             name = name[2:]
-   
+
         try:
             return super().url(name, force=True)
         except ValueError as e:
